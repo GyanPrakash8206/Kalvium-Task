c872ed85ff342c8537a02180d257e7db77a5a223
Gyan Prakash <85986487+GyanPrakash8206@users.noreply.github.com>
2023-08-29 00:13:30 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import AttendanceForm from './components/AttendanceForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceForm />
+    </div>
+  );
+}
+
+export default App;
